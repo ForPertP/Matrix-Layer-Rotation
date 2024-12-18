219d747827a6d9789f8236e99fec548bfc232d0f
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-12-19 04:53:39 +0900

    Update Matrix-Layer-Rotation.cs

--- Matrix-Layer-Rotation.cs
+++ Matrix-Layer-Rotation.cs
@@ -1,1 +1,61 @@
-Matrix-Layer-Rotation
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'matrixRotation' function below.
+     *
+     * The function accepts following parameters:
+     *  1. 2D_INTEGER_ARRAY matrix
+     *  2. INTEGER r
+     */
+
+    public static void matrixRotation(List<List<int>> matrix, int r)
+    {
+        int m = matrix.Count;
+        int n = matrix[0].Count;
+
+        List<List<int>> result = new List<List<int>>(m);
+        for (int i = 0; i < m; i++)
+        {
+            result.Add(new List<int>(new int[n]));
+        }
+    }
+}
+
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int m = Convert.ToInt32(firstMultipleInput[0]);
+
+        int n = Convert.ToInt32(firstMultipleInput[1]);
+
+        int r = Convert.ToInt32(firstMultipleInput[2]);
+
+        List<List<int>> matrix = new List<List<int>>();
+
+        for (int i = 0; i < m; i++)
+        {
+            matrix.Add(Console.ReadLine().TrimEnd().Split(' ').ToList().Select(matrixTemp => Convert.ToInt32(matrixTemp)).ToList());
+        }
+
+        Result.matrixRotation(matrix, r);
+    }
+}
